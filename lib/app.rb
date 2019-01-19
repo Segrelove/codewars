e8a881ba2ca5336e9ebb35ce3ce3cabacdb30ba3
Segrelove <mlesegret@gmail.com>
2019-01-19 15:57:44 +0100

    Array exerices

--- lib/app.rb
+++ lib/app.rb
@@ -1,1 +1,7 @@
 #Powered by Segrelove !
+
+def max_product(number, size)
+    result = number.sort {|x,y| y <=> x}
+    sum = result[0..size-1]
+    return sum.inject(:*)
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,14 @@
 require_relative '../lib/app.rb'
+
 # Free Synthaxe for ya
-describe 'scrap coin market should return someting' do 
+describe 'Basic Test' do 
     it 'should return a hash' do 
-        expect(your_method).to be_truthy
+        expect(max_product([4,3,5], 2)).to eq(20)
+        expect(max_product([10,8,7,9], 3)).to eq(720)
+        expect(max_product([8,6,4,6], 3)).to eq(288)
+        expect(max_product([10,2,3,8,1,10,4], 5)).to eq(9600)
+        expect(max_product([13,12,-27,-302,25,37,133,155,-14], 5)).to eq(247895375)
+        expect(max_product([-4,-27,-15,-6,-1], 2)).to eq(4)
+        expect(max_product([1], 1)).to eq(1)
     end
 end
