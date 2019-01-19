f3fd5083c90f3e12eb9486d8be9ffc17eecb5fee
Segrelove <mlesegret@gmail.com>
2019-01-19 20:54:17 +0100

    Last exercice to create a fframe

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,81 @@
 #Powered by Segrelove !
 
+# Playing With Lists/Arrays Series
+# Task
+# Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+# Notes
+# Array/list size is at least 3 .
+
+# Array/list's numbers Will be mixture of positives , negatives and zeros
+
+# Repeatition of numbers in the array/list could occur.
+
 def max_product(number, size)
     result = number.sort {|x,y| y <=> x}
     sum = result[0..size-1]
     return sum.inject(:*)
 end
+
+# I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 0 and 50.
+
+# Example
+# n = 3, so I expect a 3x3 square back just like below as a string:
+
+def generate_shape(n)
+    ("#{"+" * n}\n" * n).chop
+end
+
+def getIssuer(number)
+    if number.to_s[0] == "4" 
+        if number.to_s.size == 13 || number.to_s.size == 16
+            return "VISA"
+        else
+            return "Unknown"
+        end
+    elsif number.to_s[0..1] == "34" || number.to_s[0..1] == "37"
+        if number.to_s.size == 15
+            return "AMEX"
+        else
+            return "Unknown"
+        end
+    elsif number.to_s[0..3] == "6011"
+        if number.to_s.size == 16
+            return "Discover"
+        else 
+            return "Unknown"
+        end
+    elsif number.to_s[0..1].to_i.between?(51, 55) == true
+        if number.to_s.size == 16
+            return "Mastercard"
+        else 
+            return "Unknown"
+        end
+    else 
+        return "Unknownii"
+    end
+end
+
+def frame(text, char)
+    space = " "
+    number_of_words = text.count
+    sorted_by_chars = text.sort_by(&:length)
+    number_of_chars = sorted_by_chars[-1].size
+    number_of_chars_spaced = sorted_by_chars[-1].size + 4
+    part1 = char * number_of_chars_spaced + "\n"
+    part2 = []
+    i = 0
+    while i < number_of_words
+        number_of_chars_array = text[i].to_s.size
+        result = char + space + text[i]
+        x = number_of_chars - number_of_chars_array
+        pushing_result = result + (space*x) + space+ char + "\n"
+        part2.push(pushing_result)
+        i += 1
+    end
+    part3 = char * number_of_chars_spaced
+    return part1 + part2.join + part3
+    
+end
+
+puts frame(['HOCHOU','CHTO','BE', 'VSIO', 'BELLA', 'KOROCHO'], "O")
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,16 @@
 require_relative '../lib/app.rb'
 
-# Free Synthaxe for ya
-describe 'Basic Test' do 
+# Playing With Lists/Arrays Series
+# Task
+# Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+# Notes
+# Array/list size is at least 3 .
+
+# Array/list's numbers Will be mixture of positives , negatives and zeros
+
+# Repeatition of numbers in the array/list could occur.
+describe 'Array Exercice' do 
     it 'should return a hash' do 
         expect(max_product([4,3,5], 2)).to eq(20)
         expect(max_product([10,8,7,9], 3)).to eq(720)
@@ -10,5 +19,19 @@ describe 'Basic Test' do
         expect(max_product([13,12,-27,-302,25,37,133,155,-14], 5)).to eq(247895375)
         expect(max_product([-4,-27,-15,-6,-1], 2)).to eq(4)
         expect(max_product([1], 1)).to eq(1)
+    end
+end
+
+# I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 0 and 50.
+
+# Example
+# n = 3, so I expect a 3x3 square back just like below as a string:
+
+
+
+describe 'Shape Exercice' do 
+    it "should give a square of +" do
+        expect(generate_shape(3)).to eq("+++\n+++\n+++")
+        expect(generate_shape(8)).to eq("++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++")
     end
 end
